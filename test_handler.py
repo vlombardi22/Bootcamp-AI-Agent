fbabcbe8846ed4d988c8653c2bb31d6f8d6c3597
vlombardi22 <vhl2@live.com>
2023-04-24 19:39:40 -0700

    updated test handler to do tasks 1-5

--- test_handler.py
+++ test_handler.py
@@ -3,6 +3,7 @@
 # import random
 
 from viz_task45 import SailonViz as SViz
+from viz_task123 import SailonViz as TViz
 import numpy as np
 
 
@@ -17,10 +18,10 @@ class TestHandler:
         self.seed = seed
         self.domain = domain
         self.novelty = novelty
-        self.difficulty = 'easy'#difficulty
+        self.difficulty = 'easy'
         self.check = check
         self.trial_novelty = trial_novelty
-        #self.difficulty = difficulty
+        self.tdir = int(tdir)
         self.day_offset = day_offset
         self.use_img = use_img
         self.path = path
@@ -28,36 +29,36 @@ class TestHandler:
         self.use_mock = 0
         self.use_novel = 0
         self.level = 0
+
         if tdir == "5":
             self.use_novel = 1
             self.level = 208
 
 
         self.seed_list = seed_list
-        #difficulty = 'easy'
         # Load test based on params
         self.use_seed = False
+
         self.test = SViz(self.use_mock, self.use_novel, self.level, False, seed, difficulty, use_seed=self.use_seed)
-        """
-        if not check:
-            self.test1 = SViz(self.use_mock, self.use_novel, self.level, False, seed, difficulty, use_seed=self.use_seed)
-            self.test2 = AViz(self.use_mock, self.use_novel, self.level, False, seed, difficulty, use_seed=self.use_seed)#False)
-            self.test3 = HViz(self.use_mock, self.use_novel, self.level, False, seed, difficulty, use_seed=self.use_seed)
-            # self.test.reset()
-            # Get first information
-            r = np.random.randint(1, 4)
-            if r == 1:
-                self.test = self.test1  # self.test.get_state(initial=True)
-            elif r == 2:
-                self.test = self.test2
+
+        if int(tdir) < 4 :
+            if not check:
+                self.test1 = TViz(self.use_mock, self.use_novel, self.level, False, seed, difficulty, use_seed=self.use_seed, task=1)
+                self.test2 = TViz(self.use_mock, self.use_novel, self.level, False, seed, difficulty, use_seed=self.use_seed, task=2)
+                self.test3 = TViz(self.use_mock, self.use_novel, self.level, False, seed, difficulty, use_seed=self.use_seed, task=3)
+                r = np.random.randint(1, 4)
+                if r == 1:
+                    self.test = self.test1
+                elif r == 2:
+                    self.test = self.test2
+                else:
+                    self.test = self.test3
             else:
-                self.test = self.test3
-        else:
-            self.test = SViz(self.use_mock, self.use_novel, self.level, False, seed, difficulty, use_seed=self.use_seed)
-            self.test1 = None
-            self.test2 = None
-            self.test3 = None
-        """
+                self.test = SViz(self.use_mock, self.use_novel, self.level, False, seed, difficulty, use_seed=self.use_seed)
+                self.test1 = None
+                self.test2 = None
+                self.test3 = None
+
         self.information = self.test.reset()
 
 
@@ -66,30 +67,30 @@ class TestHandler:
         if self.use_seed:
             self.seed = self.seed_list[0]
         self.test = SViz(self.use_mock, self.use_novel, self.level, False, self.seed, self.difficulty, use_seed=self.use_seed)
-        """
-        self.test1 = SViz(self.use_mock, self.use_novel, self.level, False, self.seed, self.difficulty, use_seed=self.use_seed)
-        self.test2 = AViz(self.use_mock, self.use_novel, self.level, False, self.seed, self.difficulty, use_seed=self.use_seed)
-        self.test3 = HViz(self.use_mock, self.use_novel, self.level, False, self.seed, self.difficulty, use_seed=self.use_seed)
-        np.random.seed(self.seed)
-        r = np.random.randint(1, 4)
-        if r == 1:
-            self.test = self.test1  # self.test.get_state(initial=True)
-        elif r == 2:
-            self.test = self.test2
-        else:
-            self.test = self.test3
-        """
+        if self.tdir < 4:
+            self.test1 = TViz(self.use_mock, self.use_novel, self.level, False, self.seed, self.difficulty, use_seed=self.use_seed, task=1)
+            self.test2 = TViz(self.use_mock, self.use_novel, self.level, False, self.seed, self.difficulty, use_seed=self.use_seed, task=2)
+            self.test3 = TViz(self.use_mock, self.use_novel, self.level, False, self.seed, self.difficulty, use_seed=self.use_seed, task=3)
+            np.random.seed(self.seed)
+            r = np.random.randint(1, 4)
+            if r == 1:
+                self.test = self.test1  # self.test.get_state(initial=True)
+            elif r == 2:
+                self.test = self.test2
+            else:
+                self.test = self.test3
+
         self.information = self.test.reset(self.seed)
 
     def apply_action(self, action):
         action = action['action']
-        self.test.step(action)  # act(action)
+        self.test.step(action)
         self.information = self.test.get_state()
         return self.information['performance']
 
     def get_feature_vector(self):
 
-        return self.information  # ['sensors']
+        return self.information
 
     def get_feature_label(self):
         return {'action': self.information['action']}
@@ -98,21 +99,22 @@ class TestHandler:
         return self.test.is_done
 
     def reset(self, episode):
-        """
-        if not self.check:
-            if self.use_seed:
-                #self.seed_list[episode]
-                np.random.seed(self.seed_list[episode])
-            r = np.random.randint(1, 4)
-            if r == 1:
-                self.test = self.test1  # self.test.get_state(initial=True)
-            elif r == 2:
-                self.test = self.test2
-            else:
-                self.test = self.test3
-        # else:
-        #    self.test = self.test1
-        """
+        if self.tdir < 4:
+
+            if not self.check:
+                if self.use_seed:
+
+                    np.random.seed(self.seed_list[episode])
+                r = np.random.randint(1, 4)
+                if r == 1:
+                    self.test = self.test1
+                elif r == 2:
+                    self.test = self.test2
+                else:
+                    self.test = self.test3
+            else:  # TODO check if this is necessary
+               self.test = self.test1
+
         if self.use_seed:
             self.information = self.test.reset(self.seed_list[episode])
         else:
